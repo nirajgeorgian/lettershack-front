7cb900725bc5ee7407ada97fd725e8d8810fe536
jerome <josephjerome888@gmail.com>
2018-08-21 08:58:00 -0400

    book details

--- src/components/profile/profile.dashboard.js
+++ src/components/profile/profile.dashboard.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import CardContent from '@material-ui/core/CardContent'; 
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
