a11f941b8a6a69954d375c780a1f13af82f69bc2
jerome <josephjerome888@gmail.com>
2018-08-22 06:55:08 -0400

    new work

--- src/containers/works/newWork/newWork.component.js
+++ src/containers/works/newWork/newWork.component.js
@@ -9,38 +9,7 @@ import { withStyles } from '@material-ui/core';
 
 const styles = theme =>({
      
-    text:{
-        paddingBottom:20,
-        
-    },
-    divider:{
-        marginTop:20,
-        marginBottom:20
-    },
-    bootstrapRoot: {
-        padding: 0,
-        'label + &': {
-          marginTop: theme.spacing.unit * 3,
-        },
-              
-    },
-      bootstrapInput: {
-        borderRadius: 4,
-        backgroundColor: theme.palette.common.white,
-        border: '1px solid #ced4da',
-        fontSize: 16,
-        padding: '10px 12px',
-        transition: theme.transitions.create(['border-color', 'box-shadow']),
-        '&:focus': {
-          borderColor: '#80bdff',
-          boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-        },
-      },
-      bootstrapFormLabel: {
-        fontSize: 20,
-        fontWeight: 500,
-        color:'black'
-      },
+  
 })
 
 
@@ -50,55 +19,10 @@ const NewWork = (props) =>{
         <React.Fragment>
           <Grid container>
             <Grid item sm={3}>
-              <img src="https://picsum.photos/200/300" />
+             image here
             </Grid> 
             <Grid item sm={6}>
-              <Card>
-                <CardContent>
-                  <Typography>Story Details</Typography>
-                  <Divider className={classes.divider}/>
-                  <TextField
-                   className={classes.text}
-                   fullWidth={true}
-                   autoFocus={true}                   
-                   defaultValue="My first Story"
-                   label="Title"
-                   id="bootstrap-input" 
-                   InputProps={{
-                   disableUnderline: true,
-                   classes: {
-                   root: classes.bootstrapRoot,
-                   input: classes.bootstrapInput,
-                     },
-                     }}
-                   InputLabelProps={{
-                   shrink: true,
-                   className: classes.bootstrapFormLabel,
-                   }}
-                 />
-                 <Typography></Typography>
-                  <TextField
-                  className={classes.text}
-                  fullWidth={true}
-                  label="Description"
-                  id="bootstrap-input" 
-                  InputProps={{
-                  disableUnderline: true,
-                  classes: {
-                  root: classes.bootstrapRoot,
-                  input: classes.bootstrapInput,
-                     },
-                    }}
-                  InputLabelProps={{
-                  shrink: true,
-                  className: classes.bootstrapFormLabel,
-                 }}
-                   defaultValue="random description"
-                   multiline={true}
-                   rows="5"
-                  />
-                </CardContent>
-              </Card>
+              form here
             </Grid>  
           </Grid>
          </React.Fragment>
