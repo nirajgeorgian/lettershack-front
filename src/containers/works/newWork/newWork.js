6bda724920e378c3701fbd2b7d046683322fcba8
jerome <josephjerome888@gmail.com>
2018-08-23 02:40:07 -0400

    generating classname

--- src/containers/works/newWork/newWork.js
+++ src/containers/works/newWork/newWork.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
-
+import { MuiGrid } from './muiComponents';
  class NewWork extends Component{
     render(){
       const {classes} = this.props;
     return(
         <React.Fragment>
-          <Grid container>
-            <Grid item sm={3}>
+          <MuiGrid container>
+            <MuiGrid item sm={3}>
              image here
-            </Grid> 
-            <Grid item sm={6}>
+            </MuiGrid> 
+            <MuiGrid item sm={6}>
               form here
-            </Grid>  
-          </Grid>
+            </MuiGrid>  
+          </MuiGrid>
          </React.Fragment>
     )
 } 
