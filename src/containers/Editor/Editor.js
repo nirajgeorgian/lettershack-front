4f2c09a675ea6f5557223f2db34f4e31ad44aadc
rhlrj <rahulr2018@email.iimcal.ac.in>
2018-08-28 01:22:29 +0530

    editor

--- src/containers/Editor/Editor.js
+++ src/containers/Editor/Editor.js
@@ -1,30 +1,60 @@
-import React, { Component } from 'react'
-import { Editor, EditorState } from 'draft-js'
-import './editor.css'
+import React, { Component } from 'react';
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
+import editorStyles from './editorStyles.css';
+import { withStyles } from '@material-ui/core/styles';
+import './plugin.css';
 
-class TextEditor extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			editorState: EditorState.createEmpty()
-		}
-		this.onChange = editorState => {
-			this.setState({
-				editorState
-			})
-		}
-	}
-
-	render() {
-		return (
-			<div className="editor">
-				<Editor
-					editorState={this.state.editorState}
-					onChange={this.onChange}
-				/>
-			</div>
-		)
-	}
+const staticToolbarPlugin = createToolbarPlugin();
+const { Toolbar } = staticToolbarPlugin;
+const plugins = [staticToolbarPlugin];
+const text = 'The toolbar above the editor can be used for formatting text, as in conventional static editors  …';
+const styles = {
+editor :{
+ boxSizing: 'border-box',
+ border: '1px solid #ddd',
+ cursor: 'text',
+ padding: '16px',
+ borderRadius: '2px',
+ marginTop: '2em',
+ //box-shadow: inset 0px 1px 8px -3px #ABABAB,
+ background:'#fefefe'
 }
+};
 
-export default TextEditor
+class SimpleEditor extends Component {
+
+
+
+  state = {
+    editorState: createEditorStateWithText(text),
+  };
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  focus = () => {
+    this.editor.focus();
+  };
+
+  render() {
+		const {classes} = this.props;
+    return (
+      <div>
+        <div className={classes.editor} onClick={this.focus}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            plugins={plugins}
+            ref={(element) => { this.editor = element; }}
+          />
+          <Toolbar />
+        </div>
+      </div>
+    );
+  }
+}
+export default withStyles(styles)(SimpleEditor);
