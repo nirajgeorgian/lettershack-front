0a135ccecadd77850e3f1cf06db258a62ce330b1
jerome <josephjerome888@gmail.com>
2018-08-28 11:48:18 -0400

    styles fix attempt on editor

--- src/containers/Editor/Editor.js
+++ src/containers/Editor/Editor.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
 import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
 import editorStyles from './editorStyles.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { withStyles } from '@material-ui/core/styles';
 import './plugin.css';
 
@@ -9,7 +10,17 @@ const staticToolbarPlugin = createToolbarPlugin();
 const { Toolbar } = staticToolbarPlugin;
 const plugins = [staticToolbarPlugin];
 const text = 'The toolbar above the editor can be used for formatting text, as in conventional static editors  …';
-const styles = {
+
+const styles = theme =>({
+  heroUnit: {
+    backgroundColor: 'inherit'
+  },
+  heroContent: {
+    maxWidth: '600',
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+ 
 editor :{
  boxSizing: 'border-box',
  border: '1px solid #ddd',
@@ -20,7 +31,7 @@ editor :{
  //box-shadow: inset 0px 1px 8px -3px #ABABAB,
  background:'#fefefe'
 }
-};
+});
 
 class SimpleEditor extends Component {
 
@@ -43,7 +54,11 @@ class SimpleEditor extends Component {
   render() {
 		const {classes} = this.props;
     return (
-      <div>
+      <React.Fragment>
+        <CssBaseline />
+           <main>
+           <div  className={classes.heroUnit}>
+                <div className={classes.heroContent}>
         <div className={classes.editor} onClick={this.focus}>
           <Editor
             editorState={this.state.editorState}
@@ -54,6 +69,9 @@ class SimpleEditor extends Component {
           <Toolbar />
         </div>
       </div>
+     </div> 
+      </main>
+     </React.Fragment> 
     );
   }
 }
