ad0b0accc28fcc43f3b75513a629c62a56ba7f37
jerome <josephjerome888@gmail.com>
2018-08-30 07:31:12 -0400

    write button fix

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,7 @@ const store = createStore(
 	applyMiddleware(
 		ReduxThunk,
 		logger
-	),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 )
 
 export default store
